9fd466f1568616f452154d912848419d85eda3e9
Paul Woitaschek <woitaschek@posteo.de>
2017-01-15 13:32:12 +0100

    Call the functions directly without calling invoke

--- slimber/src/main/java/de/paul_woitaschek/slimber/Slimber.kt
+++ slimber/src/main/java/de/paul_woitaschek/slimber/Slimber.kt
@@ -1,50 +1,52 @@
+@file:Suppress("unused")
+
 import timber.log.Timber
 
 /** Invokes an action if any trees are planted */
 inline fun ifPlanted(action: () -> Any) {
-    if (Timber.treeCount() != 0) {
-        action.invoke()
-    }
+  if (Timber.treeCount() != 0) {
+    action()
+  }
 }
 
 /** Delegates the provided message to [Timber.e]  if any trees are planted. */
-inline fun e(message: () -> String) = ifPlanted { Timber.e(message.invoke()) }
+inline fun e(message: () -> String) = ifPlanted { Timber.e(message()) }
 
 /** Delegates the provided message to [Timber.e]  if any trees are planted. */
-inline fun e(throwable: Throwable, message: () -> String) = ifPlanted { Timber.e(throwable, message.invoke()) }
+inline fun e(throwable: Throwable, message: () -> String) = ifPlanted { Timber.e(throwable, message()) }
 
 /** Delegates the provided message to [Timber.w]  if any trees are planted. */
-inline fun w(message: () -> String) = ifPlanted { Timber.w(message.invoke()) }
+inline fun w(message: () -> String) = ifPlanted { Timber.w(message()) }
 
 /** Delegates the provided message to [Timber.w]  if any trees are planted. */
-inline fun w(throwable: Throwable, message: () -> String) = ifPlanted { Timber.w(throwable, message.invoke()) }
+inline fun w(throwable: Throwable, message: () -> String) = ifPlanted { Timber.w(throwable, message()) }
 
 /** Delegates the provided message to [Timber.i]  if any trees are planted. */
-inline fun i(message: () -> String) = ifPlanted { Timber.i(message.invoke()) }
+inline fun i(message: () -> String) = ifPlanted { Timber.i(message()) }
 
 /** Delegates the provided message to [Timber.i]  if any trees are planted. */
-inline fun i(throwable: Throwable, message: () -> String) = ifPlanted { Timber.i(throwable, message.invoke()) }
+inline fun i(throwable: Throwable, message: () -> String) = ifPlanted { Timber.i(throwable, message()) }
 
 /** Delegates the provided message to [Timber.d]  if any trees are planted. */
-inline fun d(message: () -> String) = ifPlanted { Timber.d(message.invoke()) }
+inline fun d(message: () -> String) = ifPlanted { Timber.d(message()) }
 
 /** Delegates the provided message to [Timber.d]  if any trees are planted. */
-inline fun d(throwable: Throwable, message: () -> String) = ifPlanted { Timber.d(throwable, message.invoke()) }
+inline fun d(throwable: Throwable, message: () -> String) = ifPlanted { Timber.d(throwable, message()) }
 
 /** Delegates the provided message to [Timber.v]  if any trees are planted. */
-inline fun v(message: () -> String) = ifPlanted { Timber.v(message.invoke()) }
+inline fun v(message: () -> String) = ifPlanted { Timber.v(message()) }
 
 /** Delegates the provided message to [Timber.v]  if any trees are planted. */
-inline fun v(throwable: Throwable, message: () -> String) = ifPlanted { Timber.v(throwable, message.invoke()) }
+inline fun v(throwable: Throwable, message: () -> String) = ifPlanted { Timber.v(throwable, message()) }
 
 /** Delegates the provided message to [Timber.wtf]  if any trees are planted. */
-inline fun wtf(message: () -> String) = ifPlanted { Timber.wtf(message.invoke()) }
+inline fun wtf(message: () -> String) = ifPlanted { Timber.wtf(message()) }
 
 /** Delegates the provided message to [Timber.wtf]  if any trees are planted. */
-inline fun wtf(throwable: Throwable, message: () -> String) = ifPlanted { Timber.wtf(throwable, message.invoke()) }
+inline fun wtf(throwable: Throwable, message: () -> String) = ifPlanted { Timber.wtf(throwable, message()) }
 
 /** Delegates the provided message to [Timber.log]  if any trees are planted. */
-inline fun log(priority: Int, t: Throwable, message: () -> String) = ifPlanted { Timber.log(priority, t, message.invoke()) }
+inline fun log(priority: Int, t: Throwable, message: () -> String) = ifPlanted { Timber.log(priority, t, message()) }
 
 /** Delegates the provided message to [Timber.log]  if any trees are planted. */
-inline fun log(priority: Int, message: () -> String) = ifPlanted { Timber.log(priority, message.invoke()) }
+inline fun log(priority: Int, message: () -> String) = ifPlanted { Timber.log(priority, message()) }
