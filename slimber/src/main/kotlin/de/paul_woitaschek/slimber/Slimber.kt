f72d19a5ca279ace34609ca90de72ac12ce83dd5
Paul Woitaschek <woitaschek@posteo.de>
2017-01-15 13:51:59 +0100

    Added throwable delegation methods for convenience when called from generated java file

--- slimber/src/main/kotlin/de/paul_woitaschek/slimber/Slimber.kt
+++ slimber/src/main/kotlin/de/paul_woitaschek/slimber/Slimber.kt
@@ -10,44 +10,65 @@ inline fun ifPlanted(action: () -> Any) {
   }
 }
 
-/** Delegates the provided message to [Timber.e]  if any trees are planted. */
+/** Delegates the provided message to [Timber.e] if any trees are planted. */
 inline fun e(message: () -> String) = ifPlanted { Timber.e(message()) }
 
-/** Delegates the provided message to [Timber.e]  if any trees are planted. */
+/** Delegates the provided message to [Timber.e] if any trees are planted. */
 inline fun e(throwable: Throwable, message: () -> String) = ifPlanted { Timber.e(throwable, message()) }
 
-/** Delegates the provided message to [Timber.w]  if any trees are planted. */
+/** Delegates the throwable to [Timber.e]. Made for convenience when called from java */
+fun e(throwable: Throwable): Unit = Timber.e(throwable)
+
+/** Delegates the provided message to [Timber.w] if any trees are planted. */
 inline fun w(message: () -> String) = ifPlanted { Timber.w(message()) }
 
-/** Delegates the provided message to [Timber.w]  if any trees are planted. */
+/** Delegates the provided message to [Timber.w] if any trees are planted. */
 inline fun w(throwable: Throwable, message: () -> String) = ifPlanted { Timber.w(throwable, message()) }
 
-/** Delegates the provided message to [Timber.i]  if any trees are planted. */
+/** Delegates the throwable to [Timber.w]. Made for convenience when called from java */
+fun w(throwable: Throwable): Unit = Timber.w(throwable)
+
+/** Delegates the provided message to [Timber.i] if any trees are planted. */
 inline fun i(message: () -> String) = ifPlanted { Timber.i(message()) }
 
-/** Delegates the provided message to [Timber.i]  if any trees are planted. */
+/** Delegates the provided message to [Timber.i] if any trees are planted. */
 inline fun i(throwable: Throwable, message: () -> String) = ifPlanted { Timber.i(throwable, message()) }
 
-/** Delegates the provided message to [Timber.d]  if any trees are planted. */
+/** Delegates the throwable to [Timber.i]. Made for convenience when called from java */
+fun i(throwable: Throwable): Unit = Timber.i(throwable)
+
+/** Delegates the provided message to [Timber.d] if any trees are planted. */
 inline fun d(message: () -> String) = ifPlanted { Timber.d(message()) }
 
-/** Delegates the provided message to [Timber.d]  if any trees are planted. */
+/** Delegates the provided message to [Timber.d] if any trees are planted. */
 inline fun d(throwable: Throwable, message: () -> String) = ifPlanted { Timber.d(throwable, message()) }
 
-/** Delegates the provided message to [Timber.v]  if any trees are planted. */
+/** Delegates the throwable to [Timber.d]. Made for convenience when called from java */
+fun d(throwable: Throwable): Unit = Timber.d(throwable)
+
+/** Delegates the provided message to [Timber.v] if any trees are planted. */
 inline fun v(message: () -> String) = ifPlanted { Timber.v(message()) }
 
-/** Delegates the provided message to [Timber.v]  if any trees are planted. */
+/** Delegates the provided message to [Timber.v] if any trees are planted. */
 inline fun v(throwable: Throwable, message: () -> String) = ifPlanted { Timber.v(throwable, message()) }
 
-/** Delegates the provided message to [Timber.wtf]  if any trees are planted. */
+/** Delegates the throwable to [Timber.v]. Made for convenience when called from java */
+fun v(throwable: Throwable): Unit = Timber.v(throwable)
+
+/** Delegates the provided message to [Timber.wtf] if any trees are planted. */
 inline fun wtf(message: () -> String) = ifPlanted { Timber.wtf(message()) }
 
-/** Delegates the provided message to [Timber.wtf]  if any trees are planted. */
+/** Delegates the provided message to [Timber.wtf] if any trees are planted. */
 inline fun wtf(throwable: Throwable, message: () -> String) = ifPlanted { Timber.wtf(throwable, message()) }
 
-/** Delegates the provided message to [Timber.log]  if any trees are planted. */
+/** Delegates the throwable to [Timber.wtf]. Made for convenience when called from java */
+fun wtf(throwable: Throwable): Unit = Timber.wtf(throwable)
+
+/** Delegates the provided message to [Timber.log] if any trees are planted. */
 inline fun log(priority: Int, t: Throwable, message: () -> String) = ifPlanted { Timber.log(priority, t, message()) }
 
-/** Delegates the provided message to [Timber.log]  if any trees are planted. */
+/** Delegates the provided message to [Timber.log] if any trees are planted. */
 inline fun log(priority: Int, message: () -> String) = ifPlanted { Timber.log(priority, message()) }
+
+/** Delegates the throwable to [Timber.log]. Made for convenience when called from java */
+fun log(priority: Int, throwable: Throwable): Unit = Timber.log(priority, throwable)
